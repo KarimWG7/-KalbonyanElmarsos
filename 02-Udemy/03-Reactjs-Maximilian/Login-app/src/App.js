164f26a10c246dab03d7e7f12cc1fcf3787f30c1
KarimWG7 <104060872+KarimWG7@users.noreply.github.com>
2022-07-18 15:24:36 +0200

    finishing the section and the LOGIN project

--- 02-Udemy/03-Reactjs-Maximilian/Login-app/src/App.js
+++ 02-Udemy/03-Reactjs-Maximilian/Login-app/src/App.js
@@ -3,7 +3,7 @@ import React, { Fragment, useContext } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
-import AuthContext, { AuthContextProvider } from "./context/auth-context";
+import AuthContext from "./context/auth-context";
 
 function App() {
   const ctx = useContext(AuthContext);
