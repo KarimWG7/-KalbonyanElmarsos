16142c4ce5f97b237033ebd008406a405c3bb7e0
KarimWG7 <104060872+KarimWG7@users.noreply.github.com>
2022-07-22 19:42:19 +0200

    implementing the logic of making items removable from the cart

--- 02-Udemy/03-Reactjs-Maximilian/Order-food App (S11)/src/Store/CartProvider.js
+++ 02-Udemy/03-Reactjs-Maximilian/Order-food App (S11)/src/Store/CartProvider.js
@@ -34,6 +34,30 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const exisyingCartItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - exisyingCartItem.price;
+
+    let updatedItems;
+    if (exisyingCartItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = {
+        ...exisyingCartItem,
+        amount: exisyingCartItem.amount - 1,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
   return defaultCartState;
 };
 
