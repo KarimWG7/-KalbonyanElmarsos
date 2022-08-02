d0b367d6262f650599e5d78baec93fa2e4584873
KarimWG7 <104060872+KarimWG7@users.noreply.github.com>
2022-08-02 22:47:27 +0200

    implementing the login and logout and authentication

--- 02-Udemy/03-Reactjs-Maximilian/Redux Counter(S18)/src/components/Auth.js
+++ 02-Udemy/03-Reactjs-Maximilian/Redux Counter(S18)/src/components/Auth.js
@@ -1,17 +1,36 @@
-import classes from './Auth.module.css';
+import { useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { authActions } from "../store";
+import classes from "./Auth.module.css";
 
 const Auth = () => {
+  const passwordInputRef = useRef();
+
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    if (passwordInputRef.current.value.length < 6) return;
+    dispatch(authActions.login());
+  };
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={formSubmitHandler}>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" required />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              ref={passwordInputRef}
+              required
+            />
           </div>
           <button>Login</button>
         </form>
--- 02-Udemy/03-Reactjs-Maximilian/Redux Counter(S18)/src/components/Header.js
+++ 02-Udemy/03-Reactjs-Maximilian/Redux Counter(S18)/src/components/Header.js
@@ -1,22 +1,32 @@
+import { useSelector, useDispatch } from "react-redux";
+
 import classes from './Header.module.css';
+import { authActions } from "../store";
 
 const Header = () => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
+  const loggedIn = (
+    <ul>
+      <li>
+        <a href="/">My Products</a>
+      </li>
+      <li>
+        <a href="/">My Sales</a>
+      </li>
+      <li>
+        <button onClick={logoutHandler}>Logout</button>
+      </li>
+    </ul>
+  );
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      <nav>{isAuthenticated && loggedIn}</nav>
     </header>
   );
 };
