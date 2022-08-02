508e502e4732a794a9be74b305b4fc7e93a76b02
KarimWG7 <104060872+KarimWG7@users.noreply.github.com>
2022-08-02 14:24:57 +0200

    implement the logic behind increasing and decresing the counter using redux and react redux

---
+++ 02-Udemy/03-Reactjs-Maximilian/Redux Counter(S18)/src/store/index.js
@@ -0,0 +1,15 @@
+const redux = require("redux");
+import { createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increase") {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type === "decrease") {
+    return { counter: state.counter - 1 };
+  }
+  return state;
+};
+const store = redux.createStore(counterReducer);
+
+export default store;
