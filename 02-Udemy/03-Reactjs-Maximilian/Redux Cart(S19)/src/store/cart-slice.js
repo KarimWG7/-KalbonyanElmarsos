78ef3e6b6f1dabf22c4b1d8890b13ebedc0cacd0
KarimWG7 <104060872+KarimWG7@users.noreply.github.com>
2022-08-04 02:20:22 +0200

    using action creator thunk

--- 02-Udemy/03-Reactjs-Maximilian/Redux Cart(S19)/src/store/cart-slice.js
+++ 02-Udemy/03-Reactjs-Maximilian/Redux Cart(S19)/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { cartUIActions } from "./ui-slice";
 
 const initialCartSatte = {
   items: [],
@@ -39,6 +40,49 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      cartUIActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-http-kwg-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+    };
+    try {
+      await sendRequest();
+      dispatch(
+        cartUIActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (err) {
+      dispatch(
+        cartUIActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sent cart data failed!",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 export default cartSlice.reducer;
--- 02-Udemy/03-Reactjs-Maximilian/Redux Cart(S19)/src/store/ui-slice.js
+++ 02-Udemy/03-Reactjs-Maximilian/Redux Cart(S19)/src/store/ui-slice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialUISatte = {
   cartIsVisible: false,
+  notification: null,
 };
 const uiSlice = createSlice({
   name: "ui",
@@ -9,6 +10,13 @@ const uiSlice = createSlice({
   reducers: {
     toggleCart(state) {
       state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
