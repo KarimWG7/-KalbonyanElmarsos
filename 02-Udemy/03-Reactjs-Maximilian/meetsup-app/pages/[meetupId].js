377ec89d3ecd0c40393fc51b930e17a5df31305d
KarimWG7 <104060872+KarimWG7@users.noreply.github.com>
2022-08-11 15:13:54 +0200

    finishing exploring get static paths and get server side props and get static props

--- 02-Udemy/03-Reactjs-Maximilian/meetsup-app/pages/[meetupId].js
+++ 02-Udemy/03-Reactjs-Maximilian/meetsup-app/pages/[meetupId].js
@@ -2,17 +2,51 @@ import { Fragment } from "react";
 
 import MeetupDetail from "../components/meetups/MeetupDetail";
 
-const MeetupDetails = () => {
-  const image =
-    "https://images.unsplash.com/photo-1567589602992-778bb34d417b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGFuZG1hcmtzfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=400&q=60";
+const MeetupDetails = ({meetupData}) => {
   return (
     <MeetupDetail
-      image={image}
-      title="A First Meetup"
-      address="Some Street 5, Some City"
-      description="The meetup description"
+      image={meetupData.image}
+      title={meetupData.title}
+      address={meetupData.address}
+      description={meetupData.description}
     />
   );
 };
-
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+      {
+        params: {
+          meetupId: "m3",
+        },
+      },
+    ],
+  };
+}
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://images.unsplash.com/photo-1567589602992-778bb34d417b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGFuZG1hcmtzfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=400&q=60",
+        title: "A First Meetup",
+        address: "Some Street 5, Some City",
+        description: "The meetup description",
+      },
+    },
+  };
+}
 export default MeetupDetails;
--- 02-Udemy/03-Reactjs-Maximilian/meetsup-app/pages/index.js
+++ 02-Udemy/03-Reactjs-Maximilian/meetsup-app/pages/index.js
@@ -31,6 +31,20 @@ const DUMMY_MEETUPS = [
 const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
+
+// export async function getServerSideProps(context) {
+
+//   const req = context.req
+//   const res = context.res
+
+//   return {
+//    // fetch data from an API
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   }
+// }
+
 export async function getStaticProps() {
   // fetch data from an API
   return {
