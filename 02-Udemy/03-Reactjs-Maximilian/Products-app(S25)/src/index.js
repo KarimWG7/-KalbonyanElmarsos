24b8f13e2d1e0a743a1e7789e789d8aad29d87c7
KarimWG7 <104060872+KarimWG7@users.noreply.github.com>
2022-08-12 11:34:08 +0200

    finishing converting from redux into context api

--- 02-Udemy/03-Reactjs-Maximilian/Products-app(S25)/src/index.js
+++ 02-Udemy/03-Reactjs-Maximilian/Products-app(S25)/src/index.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import productReducer from './store/reducers/products';
+import "./index.css";
+import App from "./App";
+import ProductProvider from "./context/products-context";
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
-
-const store = createStore(rootReducer);
-
-ReactDOM.render(
-  <Provider store={store}>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <ProductProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </ProductProvider>
 );
