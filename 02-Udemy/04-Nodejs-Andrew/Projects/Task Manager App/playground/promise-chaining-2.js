e74c468bed5c61e29693ee0e3656bfd9dbd48d55
KarimWG7 <104060872+KarimWG7@users.noreply.github.com>
2022-08-25 14:53:34 +0200

    implementing async await in our code

--- 02-Udemy/04-Nodejs-Andrew/Projects/Task Manager App/playground/promise-chaining-2.js
+++ 02-Udemy/04-Nodejs-Andrew/Projects/Task Manager App/playground/promise-chaining-2.js
@@ -1,14 +1,24 @@
 require("../src/db/mongoose");
 const Task = require("../src/models/task");
 
-Task.findByIdAndRemove("6306734edd7194325e3804b7")
-  .then((task) => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  })
-  .then((tasks) => {
-    console.log(tasks);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// Task.findByIdAndRemove("6306734edd7194325e3804b7")
+//   .then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then((tasks) => {
+//     console.log(tasks);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+const findTaskAndRemove = async (id) => {
+  const task = await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+  console.log(task);
+  console.log(count);
+}
+findTaskAndRemove("630508f6a2cac95c277320ce").catch(err => {
+  console.log(err)
+})
--- 02-Udemy/04-Nodejs-Andrew/Projects/Task Manager App/src/index.js
+++ 02-Udemy/04-Nodejs-Andrew/Projects/Task Manager App/src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { User } = require("./models/user");
+const User = require("./models/user");
 const Task = require("./models/task");
 require("./db/mongoose");
 
@@ -9,77 +9,71 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
   const _id = req.params.id;
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send();
-      }
-      res.send(user);
-    })
-    .catch((err) => {
-      res.status(500).send();
-    });
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send("There is no such user");
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then((tasks) => {
-      res.send(tasks);
-    })
-    .catch((err) => {
-      res.status(500).send();
-    });
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
-app.get("/tasks/:id", (req, res) => {
+app.get("/tasks/:id", async (req, res) => {
   const taskId = req.params.id;
-  Task.findById(taskId)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).send("There is no task");
-      }
-      res.send(task);
-    })
-    .catch((err) => {
-      res.status(500).send();
-    });
+  try{
+    const task = await Task.findById(taskId)
+    if(!task){
+      return res.status(404).send("there is no task")
+    }
+    res.send(task)
+  }catch(err){
+    res.status(500).send(err)
+  }
+  
 });
 
 app.listen(port, () => {
   console.log("Server is listening on https://localhost:3000");
 });
+im
