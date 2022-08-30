e2e104a6b590f7aff1bfd56e511071567e59c76d
KarimWG7 <104060872+KarimWG7@users.noreply.github.com>
2022-08-30 12:36:19 +0200

    finishing section 13

--- 02-Udemy/04-Nodejs-Andrew/Projects/Task Manager App/src/routers/task.js
+++ 02-Udemy/04-Nodejs-Andrew/Projects/Task Manager App/src/routers/task.js
@@ -17,10 +17,20 @@ router.post("/tasks", auth, async (req, res) => {
   }
 });
 
+//GET /tasks?completed=true
+//GET /tasks?completed=true&limit=2
+//GET /tasks?completed=true&limit=2&skip=2
+//GET /tasks?sortBy=createdAt_desc
+
 router.get("/tasks", auth, async (req, res) => {
   const match = {};
+  const sort = {};
   if (req.query.completed) {
     match.completed = req.query.completed === "true";
+  }
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split("_");
+    sort[parts[0]] = parts[1] === "asc" ? 1 : -1;
   }
   try {
     // const tasks = await Task.find({ owner: req.user._id });
@@ -29,7 +39,8 @@ router.get("/tasks", auth, async (req, res) => {
       match,
       options: {
         limit: parseInt(req.query.limit),
-        skip: parseInt(req.query.skip)
+        skip: parseInt(req.query.skip),
+        sort,
       },
     });
 
